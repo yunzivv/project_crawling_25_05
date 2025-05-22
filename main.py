068543559854d39d75b4c4bfaab74736d4bd259a
yunzivv <yunzivv@gmail.com>
2025-05-22 14:08:22 +0900

    full screen 캡처 도전 -> 성공

--- main.py
+++ main.py
@@ -5,9 +5,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import time
 
+service = Service('./drivers/chromedriver.exe')
 options = webdriver.ChromeOptions()
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/117 Safari/537.36")
+options.add_argument('--start-maximized')
+driver = webdriver.Chrome(service=service, options=options)
 
-driver = webdriver.Chrome(options=options)
+# url, 경로 설정
+url = 'https://www.naver.com/'
+path = 'TagName_Body-headlessOptions.png'
 
+<<<<<<< HEAD
+=======
+#실행
+driver.get(url)
+time.sleep(1)
+el = driver.find_element(By.TAG_NAME,'body')
+el.screenshot(path)
+
+# google 껐다 키기
+# service = Service('./drivers/chromedriver.exe')
+# driver = webdriver.Chrome(service = service)
+
+# driver.get("http://www.google.com")
+# print(driver.title)
+# driver.quit()
+
+# 크롤링 기본 options 설정
+# options = webdriver.ChromeOptions()
+# options.add_argument("--disable-blink-features=AutomationControlled")
+# options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/117 Safari/537.36")
+
+# driver = webdriver.Chrome(options=options)
+
+>>>>>>> 6cee9a1 (full screen 캡처 도전 -> 성공)
