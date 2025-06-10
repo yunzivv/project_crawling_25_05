ea558f174365ab9c7576a25340ecdae11d11c4ce
yunzivv <yunzivv@gmail.com>
2025-06-10 14:05:46 +0900

    엔지니어링 · 설계 > 공장엔지니어 완료

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ headers = {
     "Accept-Encoding": "gzip, deflate, br"
 }
 
-# 엔지니어링 · 설계 > 화학엔지니어 완료 451
+# 엔지니어링 · 설계 > 공장엔지니어 완료 521
 dutyCtgr = "10040"
-duty = "1000331"
+duty = "1000332"
 
 payload = {
     "condition": {
@@ -37,9 +37,9 @@ payload = {
         "dutySelect": [duty],
         "isAllDutySearch": False
     },
-    "TotalCount": 411,
+    "TotalCount": 521,
     "Page": 2,
-    "PageSize": 206
+    "PageSize": 261
 }
 
 # 세션 생성 -> headers 추가 -> POST 방식으로 요청 보내기
