4b58e9345696c73e429304b38b61183b23f019ff
yunzivv <yunzivv@gmail.com>
2025-06-19 13:13:14 +0900

    AI·개발·데이터 완료

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ headers = {
 
 # AI·개발·데이터 > 웹개발자 완료 1795
 dutyCtgr = "10031"
-duty = "1000247"
+duty = "1000423"
 
 payload = {
     "condition": {
@@ -38,9 +38,9 @@ payload = {
         "dutySelect": [duty],
         "isAllDutySearch": False
     },
-    "TotalCount": 409,
-    "Page": 2,
-    "PageSize": 205
+    "TotalCount": 146,
+    "Page": 1,
+    "PageSize": 146
 }
 
 # 세션 생성 -> headers 추가 -> POST 방식으로 요청 보내기
